8e4c186a60c9b23eefac4d9c3791f8f162fbb45b
unknown <biuerboris@gmail.com>
2018-03-27 14:56:31 +0200

    Added missing scripts

---
+++ TEXT.py
@@ -0,0 +1,25 @@
+import pygame
+
+class Text:
+
+    def __init__(self, posx, posy, string, color, font, size):
+        self.posx = posx
+        self.posy = posy
+        self.string = string
+        self.color = color
+        self.size = size
+        self.font = pygame.font.Font(font, self.size)
+        self.layout = self.font.render(self.string, True, self.color)
+        
+    def draw(self, canvas):
+        #Draw the text - Call each frame.
+        canvas.blit(self.layout,(self.posx, self.posy))
+
+    def update_string(self, string):
+        #Update the string that will be shown if needed.
+        self.layout = self.font.render(string, True, self.color)
+
+    def update_pos(self, x, y):
+        #Updates the position of the text.
+        self.posx = x
+        self.posy = y
---
+++ TEXTURES.py
@@ -0,0 +1,20 @@
+#Textures for tiles: Walls and sprites.
+import os
+
+#RYD OP I DEN HER TIL SIDST
+all_textures = [
+    #--Walls--
+    os.path.join('graphics', 'tiles', 'null.png'), #Air #0
+    os.path.join('graphics', 'tiles', 'walls', 'wood_wall.png'), #1
+    os.path.join('graphics', 'tiles', 'walls', 'maleri.png'), #2
+    os.path.join('graphics', 'tiles', 'walls', 'pejs.png'), #3
+    os.path.join('graphics', 'tiles', 'walls', 'end.png'),  #4
+    os.path.join('graphics', 'tiles', 'walls', 'bookshelf.png'), #5
+    #--Objects--
+    os.path.join('graphics', 'tiles', 'sprites', 'pillar.png'), #6
+    os.path.join('graphics', 'tiles', 'sprites', 'bord.png'), #7
+    os.path.join('graphics', 'tiles', 'sprites', 'lysekrone.png'), #8
+    #--Doors--
+    os.path.join('graphics', 'tiles', 'walls', 'Wood_door.png'), #9
+    os.path.join('graphics', 'tiles', 'walls', 'Wood_door.png'), #10
+     ]
